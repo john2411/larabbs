4ed60454898166684a59900f5478d1a5ac6ef966
john2411 <johnwoo2411@sina.com>
2018-10-14 14:22:32 +0800

    配置dingo

--- config/api.php
+++ config/api.php
@@ -10,7 +10,7 @@ return [
     | Versioning an API with Dingo revolves around content negotiation and
     | custom MIME types. A custom type will belong to one of three
     | standards trees, the Vendor tree (vnd), the Personal tree
-    | (prs), and the Unregistered tree (x).
+    | (prs), and thegit  Unregistered tree (x).
     |
     | By default the Unregistered tree (x) is used, however, should you wish
     | to you can register your type with the IANA. For more details:
